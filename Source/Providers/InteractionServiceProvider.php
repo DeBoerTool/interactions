14a39076ed08460b7faabe3e96e44d3ca77e1f70
Sanjit Singh <iamsanjit@hotmail.com>
2019-09-11 13:10:12 -0400

    Typo fix

--- Source/Providers/InteractionServiceProvider.php
+++ Source/Providers/InteractionServiceProvider.php
@@ -14,13 +14,13 @@ class InteractionServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/../../Config/interaction.php' => config_path('interaction.php'),
-        ], 'dbt-interaction-config');
+        ], 'dbt-interactions-config');
         $this->mergeConfigFrom(__DIR__ . '/../../Config/interaction.php', 'interaction');
 
 
         $timestamp = date('Y_m_d_His', time());
         $this->publishes([
             __DIR__ . '/../../Migrations/create_interactions_table.php.stub' => database_path("/migrations/{$timestamp}_create_interactions_table.php"),
-        ], 'dbt-interaction-migration');
+        ], 'dbt-interactions-migration');
     }
 }
