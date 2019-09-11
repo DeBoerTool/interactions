37d7db7e48916f135168ecb29cc5f61015b7de00
Sanjit Singh <iamsanjit@hotmail.com>
2019-09-11 12:04:51 -0400

    Adding migrations

--- Config/interaction.php
+++ Config/interaction.php
@@ -4,8 +4,14 @@ return [
 
     /*
      * This is the name of the table that will be created by the migration and
-     * used by the Activity model shipped with this package.
+     * used by the Interaction model shipped with this package.
      */
-    'table_name' => 'interaction_log'
+    'table_name' => 'interaction_log',
+
+    /*
+     * This is the database connection that will be used by the migration and
+     * the Interaction model shipped with this package.
+     */
+    'database_connection' => env('INTERACTION_LOG_DB_CONNECTION', env('DB_CONNECTION', 'mysql')),
 
 ];
--- Source/Providers/InteractionServiceProvider.php
+++ Source/Providers/InteractionServiceProvider.php
@@ -11,10 +11,16 @@ class InteractionServiceProvider extends ServiceProvider
 
     public function boot()
     {
+
         $this->publishes([
             __DIR__ . '/../Config/interaction.php' => config_path('interaction.php'),
         ], 'config');
-
         $this->mergeConfigFrom(__DIR__ . '/../Config/interaction.php', 'interaction');
+
+
+        $timestamp = date('Y_m_d_His', time());
+        $this->publishes([
+            __DIR__ . '/../Migrations/create_interactions_table.php.stub' => database_path("/migrations/{$timestamp}_create_interactions_table.php"),
+        ], 'migrations');
     }
 }
