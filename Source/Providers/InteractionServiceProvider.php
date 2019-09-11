e284b240981c53525c527eacd676ebe7bf1e18c8
Sanjit Singh <iamsanjit@hotmail.com>
2019-09-11 12:13:40 -0400

    Fix config and migration path

--- Source/Providers/InteractionServiceProvider.php
+++ Source/Providers/InteractionServiceProvider.php
@@ -13,14 +13,14 @@ class InteractionServiceProvider extends ServiceProvider
     {
 
         $this->publishes([
-            __DIR__ . '/../Config/interaction.php' => config_path('interaction.php'),
-        ], 'config');
-        $this->mergeConfigFrom(__DIR__ . '/../Config/interaction.php', 'interaction');
+            __DIR__ . '/../../Config/interaction.php' => config_path('interaction.php'),
+        ], 'dbt-interaction-config');
+        $this->mergeConfigFrom(__DIR__ . '/../../Config/interaction.php', 'interaction');
 
 
         $timestamp = date('Y_m_d_His', time());
         $this->publishes([
-            __DIR__ . '/../Migrations/create_interactions_table.php.stub' => database_path("/migrations/{$timestamp}_create_interactions_table.php"),
-        ], 'migrations');
+            __DIR__ . '/../../Migrations/create_interactions_table.php.stub' => database_path("/migrations/{$timestamp}_create_interactions_table.php"),
+        ], 'dbt-interaction-migration');
     }
 }
